4eb7257e2b10e4f66bf95586c7cfb80ceb0a325c
Brad Gross <brad.gross@bettercloud.com>
2014-12-18 16:49:10 -0500

    Formatting

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## This function creates a special "matrix" object that can cache its inverse.
+## This function creates a special "matrix" object that can cache its inverse.  
+# The "matrix" is actually a list object containing a get a set for the matrix
+# and a get a set for the matrix's inverse. Setting the matrix will clear the 
+# current inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -10,8 +10,8 @@ makeCacheMatrix <- function(x = matrix()) {
       inv <<- NULL
     }
     get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
     getinverse <- function() inv
+    setinverse <- function(inverse) inv <<- inverse
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -23,14 +23,18 @@ makeCacheMatrix <- function(x = matrix()) {
 # (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
+        ##If the inverse already exists we will return it
         if(!is.null(inv)){
           message("getting cached data")
           return(inv)
         }
         matrix <- x$get()
         inv <- solve(matrix)
+        ##Set the inverse so it's cached for next time
         x$setinverse(inv)
-        x
+        ## Return a matrix that is the inverse of 'x'
+        inv
 }
+
+#Example matrix with inverse c=rbind(c(1, -1/4), c(-1/4, 1)) 
